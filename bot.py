5765dfc7f8f2c4e0f2fedbea165e64ad5bc43fa4
Maksim Kudriavtsev <own.km.post@gmail.com>
2020-02-18 08:45:31 +0300

    Вторая версия

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 def main():
-    mybot = Updater('1045620236:AAGgIEIygAhomQvpzqepg_rHKbIUm_mhWmI', request_kwargs=PROXY)  
+    mybot = Updater("1045620236:AAGgIEIygAhomQvpzqepg_rHKbIUm_mhWmI", request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(Filters.text, talk_to_my)
     mybot.start_polling()
     mybot.idle()
+
+def greet_user(bot, update):
+    text = "Вызван старт"
+    print(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
 
 main()
